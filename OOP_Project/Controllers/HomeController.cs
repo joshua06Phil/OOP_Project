58f6223a373de36c860e752591873a7b7dc01d0b
NashM90 <macarandasn90@gmail.com>
2024-10-15 13:52:29 +0800

    AIRPLANE CESSNNA

--- OOP_Project/Controllers/HomeController.cs
+++ OOP_Project/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace OOP_Project.Controllers
             return View();
         }
 
+        public IActionResult Airplane()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
